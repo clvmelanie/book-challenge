f1036a9e5840b9fde146ef964b69dffe8129b2ab
Melanie Flaherty <melanie.e.flaherty@gmail.com>
2016-08-22 16:01:00 -0600

    Added an about page controller, modified about.html to show animage gallery with captions, made an image gallery service

--- app/services/galleryService.js
+++ app/services/galleryService.js
@@ -4,15 +4,15 @@ angular.module('CoffeeShop')
       var csGallery = [
         {
           image: "https://static.pexels.com/photos/32059/pexels-photo-large.jpg",
-          caption:""
+          caption:"Come and enjoy our bright and calm atmosphere"
         },
         {
           image: "https://static.pexels.com/photos/48665/pexels-photo-48665.jpeg",
-          caption:""
+          caption:"We have a large selection of pastries made fresh daily"
         },
         {
           image: "https://static.pexels.com/photos/6357/coffee-cup-desk-pen-large.jpg",
-          caption:""
+          caption:"A great place to get some work done"
 
         }
       ];
