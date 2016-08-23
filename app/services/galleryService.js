5ff584198181195c0253713d64653dd72ff1ff4a
Melanie Flaherty <melanie.e.flaherty@gmail.com>
2016-08-22 20:06:33 -0600

    added hover style to images and changed font on captions

--- app/services/galleryService.js
+++ app/services/galleryService.js
@@ -4,15 +4,15 @@ angular.module('CoffeeShop')
       var csGallery = [
         {
           image: "https://static.pexels.com/photos/32059/pexels-photo-large.jpg",
-          caption:"Come and enjoy our bright and calm atmosphere"
+          caption:"Come enjoy our bright and calm atmosphere"
         },
         {
           image: "https://static.pexels.com/photos/48665/pexels-photo-48665.jpeg",
-          caption:"We have a large selection of pastries made fresh daily"
+          caption:"We have a mouth-watering selection of pastries made fresh daily"
         },
         {
-          image: "https://static.pexels.com/photos/6357/coffee-cup-desk-pen-large.jpg",
-          caption:"A great place to get some work done"
+          image: "https://static.pexels.com/photos/26806/pexels-photo-large.jpg",
+          caption:"A low-key place to meet up or get some work done"
 
         }
       ];
