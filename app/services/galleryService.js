0a365ec6e75213d692fb0ee412c89b064c5fdf3d
flahertyj <flahertyj@gmail.com>
2016-09-20 10:05:29 -0600

    Set up karma/jasmine frameworks and initial tests.

--- app/services/galleryService.js
+++ app/services/galleryService.js
@@ -4,15 +4,15 @@ angular.module('CoffeeShop')
       var csGallery = [
         {
           image: "https://static.pexels.com/photos/32059/pexels-photo-large.jpg",
-          caption:"Come enjoy our bright and calm atmosphere"
+          caption: "Come enjoy our bright and calm atmosphere"
         },
         {
           image: "https://static.pexels.com/photos/48665/pexels-photo-48665.jpeg",
-          caption:"We have a mouth-watering selection of pastries made fresh daily"
+          caption: "We have a mouth-watering selection of pastries made fresh daily"
         },
         {
           image: "https://static.pexels.com/photos/26806/pexels-photo-large.jpg",
-          caption:"A low-key place to meet up or get some work done"
+          caption: "A low-key place to meet up or get some work done"
 
         }
       ];
